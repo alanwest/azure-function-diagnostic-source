60781a2191344e4ea7612242a1a845337bf2afd2
Alan West <3676547+alanwest@users.noreply.github.com>
2022-11-18 17:49:06 -0800

    System.IO.FileNotFoundException - System.Diagnostics.DiagnosticSource

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using OpenTelemetry;
+
+[assembly: FunctionsStartup(typeof(FunctionsOpenTelemetry.Startup))]
+namespace FunctionsOpenTelemetry
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var tracerProvider = Sdk.CreateTracerProviderBuilder();
+        }
+    }
+}
